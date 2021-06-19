aface3cddd4a474a38e79568188057cb44c68ab4
jose <z.mauro@live.com>
2021-06-19 15:23:07 -0300

    Add the checker if the zip code is 6 digits number.

--- yawoen-api/__init__.py
+++ yawoen-api/__init__.py
@@ -1,4 +1,5 @@
 import random
+import re
 from csv import reader
 from pymongo import MongoClient
 
@@ -19,10 +20,21 @@ def load_csv(fname='q1_catalog.csv'):
         collection_company = db_name['companies']
 
         companies = []
+
+        # Define the checker that returns 'None' if the zip code is not 6 digit
+        # It was decided to use only numbers.
+        match_zip = re.compile('^[0-9]{6}$').match
+
         for row in csv_reader:
             company_id = random.randint(0, 100)
             company_name = row[0].upper()
             zip_code = row[1]
+
+            if not match_zip(zip_code):
+                print(f"Error: the company {company_name} was not saved in the"
+                       "data base because has an invalid zip code {zip_code}")
+                continue
+
             dict_user = {'id': company_id,
                          'company_name': company_name,
                          'zip_code': zip_code}
