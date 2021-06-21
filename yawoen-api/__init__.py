b64170249e46a8e0ba1d4a56922577b7473e0b0e
jose <z.mauro@live.com>
2021-06-21 14:15:19 -0300

    Add method to load 'website' data

--- yawoen-api/__init__.py
+++ yawoen-api/__init__.py
@@ -10,14 +10,13 @@ def get_database():
 
     return client['yawoend-data']
 
-
 def load_csv(fname='q1_catalog.csv'):
     '''Load data from CSV file.'''
     db_name = get_database()
 
     with open(fname, 'r') as read_obj:
         csv_reader = reader(read_obj)
-        collection_company = db_name['companies']
+        collection = db_name['companies']
 
         companies = []
 
@@ -40,4 +39,50 @@ def load_csv(fname='q1_catalog.csv'):
                          'zip_code': zip_code}
             companies.append(dict_user)
 
-        collection_company.insert_many(companies)
+        collection.insert_many(companies)
+
+
+def integrate_website_data(fname='q2_clientData.csv'):
+    '''Load website data from file and integrate it to DB.'''
+    db_name = get_database()
+
+    with open(fname, 'r') as read_obj:
+        csv_reader = reader(read_obj)
+        collection = db_name['companies']
+
+        companies = []
+
+        # Define the checker that returns 'None' if the zip code is not 6 digit
+        # It was decided to use only numbers.
+        match_zip = re.compile('^[0-9]{6}$').match
+
+        for row in csv_reader:
+            company_name = row[0].upper()
+            zip_code = row[1]
+            website = row[2].lower()
+
+            # Fetch the company from db
+            company = collection.find_one({'company_name': company_name})
+            # If company does not exist skip this line of CSV file
+            if company is None:
+                continue
+
+            # Check if 'zip_code' is 6 digit format.
+            if not match_zip(zip_code):
+                print(f"Error: the company {company_name} was not saved in the"
+                      f" data base because has an invalid zip code {zip_code}")
+                continue
+            # Check if 'zip_code' is equal to the stored zip code
+            if zip_code != company['zip_code']:
+                print(f"Error: the website for company {company_name} was"
+                       "not saved in the data base because the zip code "
+                      f"'{zip_code}' from CSV file does not match with the "
+                      "stored zip code.")
+                continue
+
+
+            collection.update_one({"_id": company['_id'] },
+                                  {"$set": {"website": website}})
+
+
+
