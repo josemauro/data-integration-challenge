24677555ca08a796c8f4b2d2730ee3b4b86dc535
jose <z.mauro@live.com>
2021-06-22 07:49:44 -0300

    Add new tests for '_load_website_data' method

--- tests/test_yawoen-api.py
+++ tests/test_yawoen-api.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, mock
 from unittest.mock import patch, mock_open
-from yawoen_api import _load_csv
+from yawoen_api import _load_csv, _load_website_data
 
 class TestInfluxBackend(TestCase):
     """Test methods in Influx Backend."""
@@ -26,4 +26,22 @@ class TestInfluxBackend(TestCase):
         self.assertEqual(result, expected_result)
 
 
+    @patch("builtins.open", mock_open(read_data='ABCD,123456,www.abcd.com'))
+    def test_load_website_data_success(self):
+        """Unit test sucess of method '_load_website_data'."""
+        file_path = 'yawoen_api/q2_clientData.csv'
+        result = _load_website_data(file_path)
 
+        self.assertEqual(result, 'Website data loaded!')
+
+
+    @patch("os.path.exists", side_effect=[False])
+    def test_load_data_website_fail(self, mock_exists):
+        """Unit test fail case of '_load_website_data' with invalid input file."""
+        file_path = 'yawoen_api/q4_clientData.csv'
+        result = _load_website_data(file_path)
+
+        expected_result = (f"Error loading the website data from file '{file_path}'.\n"
+                "The file does not exist!")
+
+        self.assertEqual(result, expected_result)
